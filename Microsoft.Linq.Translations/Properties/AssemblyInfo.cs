85e5b34a2f1ee3a1fdb4abada9da7e473782beac
Damien Guard <damien@envytech.co.uk>
2011-09-15 11:31:15 -0700

    Initial check-in of source corresponding to NuGet package v1.0.1

---
+++ Microsoft.Linq.Translations/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.Linq.Translations")]
+[assembly: AssemblyDescription("Provides a LINQ provider independent way of translating computed properties into complex expressions.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("David Fowler, Damien Guard")]
+[assembly: AssemblyProduct("Microsoft.Linq.Translations")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3aea9788-d834-4e84-ac13-8aa2bd2fc577")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
